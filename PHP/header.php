477300e556b40dcc0a06c9b30a8b9ab6fa4a5940
Natfaji_A <modemoon2003@hotmail.com>
2021-11-08 14:58:57 +0100

    .

---
+++ PHP/header.php
@@ -0,0 +1,15 @@
+<script src="JS/navbar_active.js"></script>
+<a href="#" class="logo">ICT</a>
+<button class="toggle-collapse">
+    <i class="material-icons">list</i>
+</button>
+<nav class="navbar-collapse">
+    <ul class="navbar-nav">
+        <li class="nav-item"><a href="index.php">Who am I?</a></li>
+        <li class="nav-item"><a href="projects.php">Projects</a></li>
+        <li class="nav-item"><a href="CV.php">CV</a></li>
+        <!--<li class="nav-item"><a href="IK_3.php"></a></li>-->
+        <li class="nav-item"><a href="contact_me.php">Contact me</a></li>
+        <!-- <li id="hidden" class="nav-item"><a id="minilogo"><button type="button" id="btnTheme"><span id="btn-theme" class="material-icons">dark_mode</span></button></a></li> -->
+    </ul>
+</nav>
