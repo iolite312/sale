1052a8fa834812fd8fd518e10221629e3790c570
superman312 <68647023+superman312@users.noreply.github.com>
2021-11-18 19:01:51 +0100

    .

--- PHP/header.php
+++ PHP/header.php
@@ -2,8 +2,13 @@
     <div>
         <img class="logo" src="/img/placeholderimg.jpg" alt="placeholder">
     </div>
-    <div class="leaderboard">
-        <h2 style="text-align: center;">leaderboard</h2>
-        <p>Click <a class="text" href="../pages/leaderbords.php"> <u>here</u> </a> for detailed scoreboard</p>
+    <div class="center2">
+        <div class="leaderboard">
+            <h2 style="text-align: center;">leaderboard</h2>
+            <p>Click <a class="text" href="../pages/leaderbords.php"> <u>here</u> </a> for detailed scoreboard</p>
+        </div>
+        <div>
+
+        </div>
     </div>
 </nav>
