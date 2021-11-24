0c4765c42513cf342255a321305138de625e6064
flurbudurbur <sebastianhoogendijk@gmail.com>
2021-11-24 09:54:00 +0100

    ammend all

--- PHP/header.php
+++ PHP/header.php
@@ -1,9 +1,9 @@
 <nav class="center1">
     <div>
-        <img class="logo" src="/img/placeholderimg.jpg" alt="placeholder">
+        <img class="logo" src="./img/placeholderimg.jpg" alt="placeholder">
     </div>
     <div class="center2">
-        <div class="leaderboard">
+        <div class="leaderboard font">
             <h2 style="text-align: center;">leaderboard</h2>
             <p>Click <a class="text" href="../pages/leaderbords.php"> <u>here</u> </a> for detailed scoreboard</p>
         </div>
