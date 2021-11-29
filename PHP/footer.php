dedf7aef7ce4df99315d4104ac7e249dbef2f05c
flurbudurbur <sebastianhoogendijk@gmail.com>
2021-11-29 14:32:13 +0100

    yoink

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,14 +1,13 @@
-
 <div class="element">
     <h2>Contact Us</h2>
     <div class="highlight">
-        <img class="space" src="img/discord.svg" alt="discord">
-        <img class="space" src="img/twitter.svg" alt="twitter">
-        <img class="space" src="img/email.svg" alt="email">
-        <img class="space" src="img/github.svg" alt="github">
+        <a class="space" href=""><img class="linky-px" src="img/discord.svg" alt="discord"></a>
+        <a class="space" href=""><img class="linky-px" src="img/twitter.svg" alt="twitter"></a>
+        <a class="space" href=""><img class="linky-px" src="img/email.svg" alt="email"></a>
+        <a class="space" href="https://github.com/superman312/sale"><img class="linky-px" src="img/github.svg" alt="github"></a>
     </div>
 </div>
 <div class="element">
     <h2>&copy; Copyright</h2>
-    <p class="highlight">All rights reserved.</p>
+    <p class="highlight wide">All rights reserved.</p>
 </div>
