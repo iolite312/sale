9eb93ab29f99cbf225260c0e6aa2d47595bad7d5
flurbudurbur <sebastianhoogendijk@gmail.com>
2021-11-30 14:50:07 +0100

    screee

--- PHP/header.php
+++ PHP/header.php
@@ -1,7 +1,5 @@
 <nav class="header-center">
-    <div class="pos-l">
-        <img id="logo" src="./img/templogo.png" alt="Website Logo">
-    </div>
+    <img class="logo pos-l" src="./img/templogo.png" alt="Website Logo">
     <h1 class="header-title"><?php echo "$title"; ?></h1>
     <div class="lb-link font pos-r">
         <a class="link-dec font-w" href="leaderboards.php" target="_blank">
