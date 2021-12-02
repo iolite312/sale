1fcb0da2bd861e2f80ee0fee59d2753424372b6c
superman312 <68647023+superman312@users.noreply.github.com>
2021-12-02 10:57:56 +0100

    db finish

---
+++ PHP/login.php
@@ -0,0 +1,47 @@
+<?php
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'unityaccess';
+    $con = mysqli_connect($servername,$username, $password, $dbname);
+    //check that connection happend
+    if(mysqli_connect_errno())
+    {
+        echo "1: Connection Failed"; //error code #1 - connection failed
+        exit();
+    }
+
+    $username = $_POST["name"];
+    $password = $_POST["password"];
+
+    //check if name exists
+    $namecheckquery = "SELECT username, salt, hash, score FROM players WHERE username='" . $username . "';";
+   
+    $namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed"); //error code #2 - namecheckquery failed
+    if (mysqli_num_rows($namecheck) != 1)
+    {
+        echo "5: Either no user with name, or more than one"; //error code #5 - number of names matching !=1
+        exit();
+    }
+
+    //get login info from query
+    $existinginfo = mysqli_fetch_assoc($namecheck);
+    $salt = $existinginfo["salt"];
+    $hash = $existinginfo["hash"];
+
+    // echo "$username, $password\t";
+
+    $loginhash = crypt($password, $salt);
+    if ($hash != $loginhash)
+    {
+        echo "6: Incorrect password"; //error code #6 - password does not match hash
+        exit();
+    }
+
+    echo "0\t" . $existinginfo["score"];
+
+
+
+
+?>
---
+++ PHP/savedata.php
@@ -0,0 +1,34 @@
+<?php
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'unityaccess';
+    $con = mysqli_connect($servername,$username, $password, $dbname);
+    //check that connection happend
+    if(mysqli_connect_errno())
+    {
+        echo "1: Connection Failed"; //error code #1 - connection failed
+        exit();
+    }
+    
+    $username = $_POST["name"];
+    $newscore = $_POST["score"];
+
+
+    //double check there is only one user with this name
+    $namecheckquery = "SELECT username FROM players WHERE username='" . $username . "';";
+
+    $namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed"); //error code #2 - namecheckquery failed
+    if (mysqli_num_rows($namecheck) != 1)
+    {
+        echo "5: Either no user with name, or more than one"; //error code #5 - number of names matching !=1
+        exit();
+    }
+
+    $updatequery = "UPDATE players SET score = " . $newscore . " WHERE username = '" . $username . "';";
+    mysqli_query($con, $updatequery) or die("7: Save query failed"); // error code #7 - Update query failed
+
+    echo "0";
+
+?>
