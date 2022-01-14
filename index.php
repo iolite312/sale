7e420ddedbd14adc673c7bf59e3da1c5b2f5bd73
superman312 <68647023+superman312@users.noreply.github.com>
2022-01-14 11:22:13 +0100

    formatted and footer clip fix again

--- index.php
+++ index.php
@@ -1,118 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Come play our brand-new game Goblin Tower Defence and climb the leaderboards!">
-    <link rel="stylesheet" href="CSS/global.css">
-    <link rel="stylesheet" href="CSS/index.css">
-    <link rel="stylesheet" href="CSS/header.css">
-    <link rel="stylesheet" href="CSS/footer.css">
-    <link rel="stylesheet" href="./game/TemplateData/style.css">
-    <link rel="icon" href="./img/placeholderimg.jpg" type="image/png" sizes="32x32">
-    <title>Goblin Tower Defence</title>
-    <?php $title = "Goblin Tower Defence"; ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Come play our brand-new game Goblin Tower Defence and climb the leaderboards!">
+  <link rel="stylesheet" href="CSS/global.css">
+  <link rel="stylesheet" href="CSS/index.css">
+  <link rel="stylesheet" href="CSS/header.css">
+  <link rel="stylesheet" href="CSS/footer.css">
+  <link rel="stylesheet" href="./game/TemplateData/style.css">
+  <link rel="icon" href="./img/placeholderimg.jpg" type="image/png" sizes="32x32">
+  <title>Goblin Tower Defence</title>
+  <?php $title = "Goblin Tower Defence"; ?>
 </head>
+
 <body>
-    <header>
-        <?php include 'PHP/header.php'; ?>
-    </header>
+  <header>
+    <?php include 'PHP/header.php'; ?>
+  </header>
+  <div>
     <main class="test">
-        <div class="center">
+      <div class="center">
         <div id="unity-container" class="unity-desktop">
-      <canvas id="unity-canvas" width=960 height=600 style="margin-top:150px"></canvas>
-      <div id="unity-loading-bar">
-        <div id="unity-logo"></div>
-        <div id="unity-progress-bar-empty">
-          <div id="unity-progress-bar-full"></div>
+          <canvas id="unity-canvas" width=960 height=600 style="margin-top:150px"></canvas>
+          <div id="unity-loading-bar">
+            <div id="unity-logo"></div>
+            <div id="unity-progress-bar-empty">
+              <div id="unity-progress-bar-full"></div>
+            </div>
+          </div>
+          <div id="unity-warning"> </div>
         </div>
       </div>
-      <div id="unity-warning"> </div>
-      </div>
-    </div>
-    <script>
-      var container = document.querySelector("#unity-container");
-      var canvas = document.querySelector("#unity-canvas");
-      var loadingBar = document.querySelector("#unity-loading-bar");
-      var progressBarFull = document.querySelector("#unity-progress-bar-full");
-      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-      var warningBanner = document.querySelector("#unity-warning");
+      <script>
+        var container = document.querySelector("#unity-container");
+        var canvas = document.querySelector("#unity-canvas");
+        var loadingBar = document.querySelector("#unity-loading-bar");
+        var progressBarFull = document.querySelector("#unity-progress-bar-full");
+        var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+        var warningBanner = document.querySelector("#unity-warning");
 
-      // Shows a temporary message banner/ribbon for a few seconds, or
-      // a permanent error message on top of the canvas if type=='error'.
-      // If type=='warning', a yellow highlight color is used.
-      // Modify or remove this function to customize the visually presented
-      // way that non-critical warnings and error messages are presented to the
-      // user.
-      function unityShowBanner(msg, type) {
-        function updateBannerVisibility() {
-          warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+        // Shows a temporary message banner/ribbon for a few seconds, or
+        // a permanent error message on top of the canvas if type=='error'.
+        // If type=='warning', a yellow highlight color is used.
+        // Modify or remove this function to customize the visually presented
+        // way that non-critical warnings and error messages are presented to the
+        // user.
+        function unityShowBanner(msg, type) {
+          function updateBannerVisibility() {
+            warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+          }
+          var div = document.createElement('div');
+          div.innerHTML = msg;
+          warningBanner.appendChild(div);
+          if (type == 'error') div.style = 'background: red; padding: 10px;';
+          else {
+            if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+            setTimeout(function() {
+              warningBanner.removeChild(div);
+              updateBannerVisibility();
+            }, 0.1);
+          }
+          updateBannerVisibility();
         }
-        var div = document.createElement('div');
-        div.innerHTML = msg;
-        warningBanner.appendChild(div);
-        if (type == 'error') div.style = 'background: red; padding: 10px;';
-        else {
-          if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-          setTimeout(function() {
-            warningBanner.removeChild(div);
-            updateBannerVisibility();
-          }, 0.1);
+
+        var buildUrl = "./game/Build";
+        var loaderUrl = buildUrl + "/game.loader.js";
+        var config = {
+          dataUrl: buildUrl + "/game.data",
+          frameworkUrl: buildUrl + "/game.framework.js",
+          codeUrl: buildUrl + "/game.wasm",
+          streamingAssetsUrl: "StreamingAssets",
+          companyName: "DefaultCompany",
+          productName: "SALE",
+          productVersion: "0.1 Alpha",
+          showBanner: unityShowBanner,
+        };
+
+        // By default Unity keeps WebGL canvas render target size matched with
+        // the DOM size of the canvas element (scaled by window.devicePixelRatio)
+        // Set this to false if you want to decouple this synchronization from
+        // happening inside the engine, and you would instead like to size up
+        // the canvas DOM size and WebGL render target sizes yourself.
+        // config.matchWebGLToCanvasSize = false;
+
+        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+          container.className = "unity-mobile";
+          // Avoid draining fillrate performance on mobile devices,
+          // and default/override low DPI mode on mobile browsers.
+          config.devicePixelRatio = 1;
+          unityShowBanner('WebGL builds are not supported on mobile devices.');
+        } else {
+          canvas.style.width = "75vw";
+          canvas.style.height = "85vh";
         }
-        updateBannerVisibility();
-      }
+        loadingBar.style.display = "block";
 
-      var buildUrl = "./game/Build";
-      var loaderUrl = buildUrl + "/game.loader.js";
-      var config = {
-        dataUrl: buildUrl + "/game.data",
-        frameworkUrl: buildUrl + "/game.framework.js",
-        codeUrl: buildUrl + "/game.wasm",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "DefaultCompany",
-        productName: "SALE",
-        productVersion: "0.1 Alpha",
-        showBanner: unityShowBanner,
-      };
-
-      // By default Unity keeps WebGL canvas render target size matched with
-      // the DOM size of the canvas element (scaled by window.devicePixelRatio)
-      // Set this to false if you want to decouple this synchronization from
-      // happening inside the engine, and you would instead like to size up
-      // the canvas DOM size and WebGL render target sizes yourself.
-      // config.matchWebGLToCanvasSize = false;
-
-      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        container.className = "unity-mobile";
-        // Avoid draining fillrate performance on mobile devices,
-        // and default/override low DPI mode on mobile browsers.
-        config.devicePixelRatio = 1;
-        unityShowBanner('WebGL builds are not supported on mobile devices.');
-      } else {
-        canvas.style.width = "75vw";
-        canvas.style.height = "85vh";
-      }
-      loadingBar.style.display = "block";
-
-      var script = document.createElement("script");
-      script.src = loaderUrl;
-      script.onload = () => {
-        createUnityInstance(canvas, config, (progress) => {
-          progressBarFull.style.width = 100 * progress + "%";
-        }).then((unityInstance) => {
-          loadingBar.style.display = "none";
-        }).catch((message) => {
-          alert(message);
-        });
-      };
-      document.body.appendChild(script);
-    </script>
-        </div>
+        var script = document.createElement("script");
+        script.src = loaderUrl;
+        script.onload = () => {
+          createUnityInstance(canvas, config, (progress) => {
+            progressBarFull.style.width = 100 * progress + "%";
+          }).then((unityInstance) => {
+            loadingBar.style.display = "none";
+          }).catch((message) => {
+            alert(message);
+          });
+        };
+        document.body.appendChild(script);
+      </script>
     </main>
+  </div>
+  <div class="footercenter">
     <footer>
-        <?php include 'PHP/footer.php'; ?>
+      <?php include 'PHP/footer.php'; ?>
     </footer>
+  </div>
 </body>
 
 </html>
--- leaderboards.php
+++ leaderboards.php
@@ -1,167 +1,169 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="placeholder">
-        <link rel="stylesheet" href="CSS/global.css">
-        <link rel="stylesheet" href="CSS/footer.css">
-        <link rel="stylesheet" href="CSS/header.css">
-        <link rel="stylesheet" href="CSS/leaderboard.css">
-        <link rel="icon" href="./img/placeholderimg.jpg" type="image/png" sizes="32x32">
-        <title>Leaderboard</title>
-        <?php $title = "Leaderboard"; ?>
-    </head>
-    <script>
 
-        //gets difficulty from the difficulty dropdown menu 
-        function getDiff(difficultySelect){
-            var val = difficultySelect.value
-            document.cookie = "difficulty=" + val;
-            location.reload()
-        }
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="placeholder">
+    <link rel="stylesheet" href="CSS/global.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/leaderboard.css">
+    <link rel="icon" href="./img/placeholderimg.jpg" type="image/png" sizes="32x32">
+    <title>Leaderboard</title>
+    <?php $title = "Leaderboard"; ?>
+</head>
+<script>
+    //gets difficulty from the difficulty dropdown menu 
+    function getDiff(difficultySelect) {
+        var val = difficultySelect.value
+        document.cookie = "difficulty=" + val;
+        location.reload()
+    }
 
-        //gets the cookie that is asked for with the parameter needed in the function
-        function getCookie(cookie){
-            name = cookie + "=";
-            ca = document.cookie.split(';');
-            for (i=0; i < ca.length; i++){
-                c = ca[i];
-                //while there is a space in the string it will remove the space from the string
-                while (c.charAt(0) == ' '){
-                    c = c.substring(1);
-                }
-                //if the specified cookie exists return the value of the cookie else return null
-                if(c.indexOf(name) == 0){
-                    return c.substring(name.length, c.length);
-                }else{
-                    return null;
-                }
+    //gets the cookie that is asked for with the parameter needed in the function
+    function getCookie(cookie) {
+        name = cookie + "=";
+        ca = document.cookie.split(';');
+        for (i = 0; i < ca.length; i++) {
+            c = ca[i];
+            //while there is a space in the string it will remove the space from the string
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            //if the specified cookie exists return the value of the cookie else return null
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            } else {
+                return null;
             }
         }
+    }
 
-        //this sets the remembered difficulty value from the cookie into the dropdown menu
-        window.onload = function() {
-            if(getCookie("difficulty") === null){
-                document.getElementById("diff").value= parseInt('1');
-            }else{
-                difficulty = getCookie("difficulty");
-                document.getElementById("diff").value= parseInt(difficulty);
-            }
+    //this sets the remembered difficulty value from the cookie into the dropdown menu
+    window.onload = function() {
+        if (getCookie("difficulty") === null) {
+            document.getElementById("diff").value = parseInt('1');
+        } else {
+            difficulty = getCookie("difficulty");
+            document.getElementById("diff").value = parseInt(difficulty);
         }
+    }
 
-        //this sets the timeFrame cookie value for the specific scoreboard with value from the clicked button
-        function setTimeFrame(timeFrame, scoreboard){
-            switch(scoreboard){
-                case 'score':
-                    document.cookie = "timeFrameScores=" + timeFrame;
-                    break;
-                case 'waves':
-                    document.cookie = "timeFrameWaves=" + timeFrame;
-                    break;
-                case 'kills':
-                    document.cookie = "timeFrameKills=" + timeFrame;
-                    break;
-            }
-            location.reload();
+    //this sets the timeFrame cookie value for the specific scoreboard with value from the clicked button
+    function setTimeFrame(timeFrame, scoreboard) {
+        switch (scoreboard) {
+            case 'score':
+                document.cookie = "timeFrameScores=" + timeFrame;
+                break;
+            case 'waves':
+                document.cookie = "timeFrameWaves=" + timeFrame;
+                break;
+            case 'kills':
+                document.cookie = "timeFrameKills=" + timeFrame;
+                break;
         }
-    </script>
-    <body>
-        <header>
-            <?php include "PHP/lb-header.php"; ?>
-        </header>
-        <div id="difficulty-selector-mobile">
-            <form action="">
-                <select name="difficulty" id="difficulty" onChange="getDiff(this.value)">
-                    <option value="easy">Easy</option>
-                    <option value="normal">Normal</option>
-                    <option value="hard">Hard</option>
-                    <option value="impossible">Impossible</option>
-                </select>
-            </form>
-        </div>
-        <div class="column">
-            <!-- Leaderboards -->
-            <div class="leaderboard shadow-l">
-                <!-- Kill Count Leaderboard -->
-                <h1 class="lb-title">Kills</h1>
-                <div class="placeholder-lb">
-                    <div class="scoreboard">
-                        <!-- Scores Placeholder -->
-                        <?php include 'PHP/scoresKills.php'; ?>
-                    </div>
-                    <div class="personal">
-                        <div class="personal-center">
-                            <h2>Your Score</h2>
-                                <!-- Your Score Placeholder -->
-                            <?php include 'PHP/personalScoreKills.php'; ?>
-                        </div>
-                        <div class="buttons-lb">
-                            <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'kills')">Daily</a>
-                            <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'kills')">Monthly</a>
-                            <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'kills')">All-Time</a>
-                        </div>
-                    </div>
+        location.reload();
+    }
+</script>
+
+<body>
+    <header>
+        <?php include "PHP/lb-header.php"; ?>
+    </header>
+    <div id="difficulty-selector-mobile">
+        <form action="">
+            <select name="difficulty" id="difficulty" onChange="getDiff(this.value)">
+                <option value="easy">Easy</option>
+                <option value="normal">Normal</option>
+                <option value="hard">Hard</option>
+                <option value="impossible">Impossible</option>
+            </select>
+        </form>
+    </div>
+    <div class="column">
+        <!-- Leaderboards -->
+        <div class="leaderboard shadow-l">
+            <!-- Kill Count Leaderboard -->
+            <h1 class="lb-title">Kills</h1>
+            <div class="placeholder-lb">
+                <div class="scoreboard">
+                    <!-- Scores Placeholder -->
+                    <?php include 'PHP/scoresKills.php'; ?>
                 </div>
-            </div>
-            <div class="leaderboard shadow-m">
-                <!-- Wave Count Leaderboard -->
-                <h1 class="lb-title">Waves</h1>
-                <div class="placeholder-lb">
-                    <div class="scoreboard">
-                        <!-- Scores Placeholder -->
-                        <?php include 'PHP/scoresWaves.php'; ?>
+                <div class="personal">
+                    <div class="personal-center">
+                        <h2>Your Score</h2>
+                        <!-- Your Score Placeholder -->
+                        <?php include 'PHP/personalScoreKills.php'; ?>
                     </div>
-                    <div class="personal">
-                        <div class="personal-center">
-                            <h2>Your Score</h2>
-                                <!-- Your Score Placeholder -->
-                            <?php include 'PHP/personalScoreWaves.php'; ?>
-                        </div>
-                        <div class="buttons-lb">
-                            <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'waves')">Daily</a>
-                            <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'waves')">Monthly</a>
-                            <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'waves')">All-Time</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="leaderboard shadow-r">
-                <!-- Score Count Leaderboard -->
-                <h1 class="lb-title">Score</h1>
-                <div class="placeholder-lb">
-                    <div class="scoreboard">
-                        <!-- Scores Placeholder -->
-                        <?php include 'PHP/scores.php'; ?>
-                    </div>
-                    <div class="personal">
-                        <div class="personal-center">
-                            <h2>Your Score</h2>
-                                <!-- Your Score Placeholder -->
-                            <?php include 'PHP/personalScore.php'; ?>
-                        </div>
-                        <div class="buttons-lb">
-                            <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'score')">Daily</a>
-                            <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'score')">Monthly</a>
-                            <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'score')">All-Time</a>
-                        </div>
+                    <div class="buttons-lb">
+                        <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'kills')">Daily</a>
+                        <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'kills')">Monthly</a>
+                        <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'kills')">All-Time</a>
                     </div>
                 </div>
             </div>
         </div>
-        <div id="difficulty-selector">
-            <form action="">
-                <select name="difficulty" id="diff" onChange="getDiff(this)">
-                    <option value="1">Easy</option>
-                    <option value="2">Normal</option>
-                    <option value="3">Hard</option>
-                    <option value="4">Impossible</option>
-                </select>
-            </form>
+        <div class="leaderboard shadow-m">
+            <!-- Wave Count Leaderboard -->
+            <h1 class="lb-title">Waves</h1>
+            <div class="placeholder-lb">
+                <div class="scoreboard">
+                    <!-- Scores Placeholder -->
+                    <?php include 'PHP/scoresWaves.php'; ?>
+                </div>
+                <div class="personal">
+                    <div class="personal-center">
+                        <h2>Your Score</h2>
+                        <!-- Your Score Placeholder -->
+                        <?php include 'PHP/personalScoreWaves.php'; ?>
+                    </div>
+                    <div class="buttons-lb">
+                        <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'waves')">Daily</a>
+                        <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'waves')">Monthly</a>
+                        <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'waves')">All-Time</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <footer>
-            <?php include 'PHP/footer.php'; ?>
-        </footer>
-    </body>
+        <div class="leaderboard shadow-r">
+            <!-- Score Count Leaderboard -->
+            <h1 class="lb-title">Score</h1>
+            <div class="placeholder-lb">
+                <div class="scoreboard">
+                    <!-- Scores Placeholder -->
+                    <?php include 'PHP/scores.php'; ?>
+                </div>
+                <div class="personal">
+                    <div class="personal-center">
+                        <h2>Your Score</h2>
+                        <!-- Your Score Placeholder -->
+                        <?php include 'PHP/personalScore.php'; ?>
+                    </div>
+                    <div class="buttons-lb">
+                        <a id="lb-button" class="daily button" onclick="setTimeFrame('day', 'score')">Daily</a>
+                        <a id="lb-button" class="monthly button" onclick="setTimeFrame('month', 'score')">Monthly</a>
+                        <a id="lb-button" class="alltime button" onclick="setTimeFrame('all', 'score')">All-Time</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="difficulty-selector">
+        <form action="">
+            <select name="difficulty" id="diff" onChange="getDiff(this)">
+                <option value="1">Easy</option>
+                <option value="2">Normal</option>
+                <option value="3">Hard</option>
+                <option value="4">Impossible</option>
+            </select>
+        </form>
+    </div>
+    <footer>
+        <?php include 'PHP/footer.php'; ?>
+    </footer>
+</body>
+
 </html>
